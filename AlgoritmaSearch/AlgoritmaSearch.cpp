e922b72a1591b41489ea4c70ddb8908f17d38326
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2024-04-05 11:23:08 +0700

    pilihan untuk lanjut mencari atau tidak

--- AlgoritmaSearch/AlgoritmaSearch.cpp
+++ AlgoritmaSearch/AlgoritmaSearch.cpp
@@ -64,6 +64,7 @@ void LinearSearch()
 		if (i == n)
 			cout << "\n" << item << "not found in the array\n";
 		cout << "\nNumber of comparisons: " << ctr << endl;
+		cout << "\nContinue search (y/n): ";
 
 
 	}
